050f1df51b4659190501d6bb57c91fbe9ccc26f7
Ereklebazanovi <erekle.bazanovi.1@iliauni.edu.ge>
2024-08-07 22:52:32 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let temperature = 27;
+
+    if (temperature<0 ) {
+    console.log("ყინავს")
+}
+else if (temperature > -1 && temperature < 11){
+    console.log("ძალიან ცივა")
+}
+else if (temperature > 10 && temperature < 21) {
+    console.log("ცივა")
+}
+else if (temperature > 20 && temperature < 31){
+    console.log("თბილა")
+}
+else {
+    console.log("ცხელა")
+};
